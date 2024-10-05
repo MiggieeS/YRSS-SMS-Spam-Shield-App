d64fce29e5000ba89e9c2ca8bbd90bc29da6ee5b
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2024-10-05 22:46:41 +0800

    Remove duplicate rows and make sure there are equal number of spam and non-spam messages in the data set

--- python/spam_logistic_regression_model.py
+++ python/spam_logistic_regression_model.py
@@ -13,17 +13,23 @@ import sys
 column_headers = ["Text Message", "Spam Classification"]
 
 # Read CSV from Google Sheets.
-df = pd.read_csv("https://docs.google.com/spreadsheets/d/1sq05T7vcrl-CeSGqVxVVeGQjdYpbaV-uuXbFO92RxgU/export?format=csv", usecols=[0, 1])
+# df = pd.read_csv("https://docs.google.com/spreadsheets/d/1sq05T7vcrl-CeSGqVxVVeGQjdYpbaV-uuXbFO92RxgU/export?format=csv", usecols=[0, 1])
+df = pd.read_csv("https://docs.google.com/spreadsheets/d/16JprumWE9baoRkWjQ20yrTCMgxFkJ5FJh_yLRRRun9g/export?format=csv", usecols=[0, 1])
 
 # Remove rows with missing data.
 df.dropna(subset=column_headers, inplace=True, how="any")
 
+# Remove duplicate rows (except the first occurence).
+df.drop_duplicates(subset=column_headers)
+
+# Make sample size have a 1:1 ratio (i.e., for every spam message there is a non spam message)
+spam_df = df.query(f'(`{column_headers[1]}` == 1)')
+non_spam_df = df.query(f'(`{column_headers[1]}` == 0)')
 # Use random sample_size*2 rows (for testing purpose).
 # Comment out when not testing.
 # sample_size = 80
-# spam_df = df.query(f'(`{column_headers[1]}` == 1)').sample(sample_size, random_state=0)
-# non_spam_df = df.query(f'(`{column_headers[1]}` == 0)').sample(sample_size, random_state=0)
-# df = pd.concat([spam_df, non_spam_df], ignore_index=True)
+sample_size = min(spam_df.shape[0], non_spam_df.shape[0])
+df = pd.concat([spam_df.sample(sample_size, random_state=0), non_spam_df.sample(sample_size, random_state=0)], ignore_index=True)
 
 # Preprocess "Text Message" column.
 # Download stop words from NLTK.
@@ -32,8 +38,8 @@ nltk.download("stopwords")
 def preprocess_text_message(text_message):
     # Remove punctuation in text_message.
     text_message = text_message.translate(str.maketrans("", "", string.punctuation))
-    # Remove stop words (from NLTK) in text_message.
-    text_message = [word for word in text_message.split() if word.lower() not in stopwords.words("english")]
+    # Remove stop words (from NLTK) in text_message and turn words to lower case.
+    text_message = [word.lower() for word in text_message.split() if word.lower() not in stopwords.words("english")]
     return " ".join(text_message)
 
 df[column_headers[0]] = df[column_headers[0]].apply(preprocess_text_message)
@@ -46,8 +52,8 @@ X = df[column_headers[0]].values
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
 # Vectorize X_train and X_test.
-# Adding stop_words="english" may be redundant.
-vectorizer = TfidfVectorizer(stop_words="english")
+# Adding stop_words="english" and lowercase=True may be redundant.
+vectorizer = TfidfVectorizer(stop_words="english", lowercase=True)
 X_train_vector = vectorizer.fit_transform(X_train)
 X_test_vector = vectorizer.transform(X_test)
 
