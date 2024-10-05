aa85b2cbfb8e38c8b70a76296c58b54aace2aabe
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2024-10-05 16:16:13 +0800

    Remove unnecessary permission and function

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,11 @@ import 'package:flutter/services.dart';
 
 final Telephony telephony = Telephony.instance;
 
-void backgrounMessageHandler(SmsMessage message) async {
-  // Handle background SMS message
-  print("Received SMS in background: ${message.body}");
-}
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Request SMS permissions before starting the app
-  bool? permissionsGranted = await telephony.requestPhoneAndSmsPermissions;
+  bool? permissionsGranted = await telephony.requestSmsPermissions;
 
   // set to portrait mode only
   await SystemChrome.setPreferredOrientations([
